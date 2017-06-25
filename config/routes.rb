33bb8aa68fc2e71d0c3f2f127c007074dcc0c496
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2017-06-25 13:46:58 -0500

    Generate product, category, and brand scaffolds and place them under the admin namespace.
    Edit routes.rb to scope to the admin namespace so that we can keep our URL and Path helpers.
    Preface product, category, and brand controllers with Admin::
    Create controllers/admin directory and put product, category, and brand controllers within it to get proper scoping.
    Create views/admin directory and put product, category, and brand view folders within it.
    Create root route to admin/products#index
    Run migrations for product, category, and brand

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  scope module: 'admin', path: 'admin' do
+    resources :products
+    resources :brands
+    resources :categories
+  end
+
   # TODO: Define a root url per Devise instructions
-  # root 'some_controller#some_view'
+  root 'admin/products#index'
 end
