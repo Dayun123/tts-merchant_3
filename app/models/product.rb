1de01c545e3480abbe6f8cbb52f86c387ebd2eb8
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2017-06-25 16:15:10 -0500

    Paperclip setup.
    Add a Paperclip avatar to products table with rails g migration Product avatar.
    Whitelist :avatar in the Products controller.
    Add Paperclip methods has_attached_file and validates_content_type to Product model, adjusted to serve from the asset pipeline instead of public.
    Add a file upload input to the product form partial.
    Add image_tag that shows the product avatar to views/admin/products/show.html.erb
    
    Add irbtools to Gemfile.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,10 @@
 class Product < ApplicationRecord
   belongs_to :category
   belongs_to :brand
+
+  # Paperclip setup. The :avatar field will represent an image of the Product. :style_missing.png will translate to medium_missing.png, thumb_missing.png, and original_missing.png. You get access to these through image_tag @product.avatar.url(:thumb/:medium) if you don't pass in a param to url, it will assume you want the :original. The docs say to use a path like '/images/:style/missing.png' but this doesn't work, something to do with the way the asset pipeline works. You'll see a path in the image tag that is like '/images/medium/missing.png' and you can have that path and file in your app/assets/images/medium/ directory, but the asset pipeline won't find it. I guess leaving off /images is the way to go, as it somehow knows to look there by default.
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: ":style_missing.png"
+
+  # Validates that the content_type of the :avatar attribute of this product is an image.
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 end
