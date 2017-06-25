fed97824f56a59563c16ebd6762e7ce57d27b0cc
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2017-06-25 16:40:01 -0500

    Ran annotate to annotate models.

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,3 +8,27 @@ class Product < ApplicationRecord
   # Validates that the content_type of the :avatar attribute of this product is an image.
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 end
+
+# == Schema Information
+#
+# Table name: products
+#
+#  id                  :integer          not null, primary key
+#  name                :string
+#  price               :decimal(, )
+#  quantity            :integer
+#  description         :text
+#  category_id         :integer
+#  brand_id            :integer
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#  avatar_file_name    :string
+#  avatar_content_type :string
+#  avatar_file_size    :integer
+#  avatar_updated_at   :datetime
+#
+# Indexes
+#
+#  index_products_on_brand_id     (brand_id)
+#  index_products_on_category_id  (category_id)
+#
