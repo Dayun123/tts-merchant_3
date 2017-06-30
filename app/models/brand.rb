709b009037e6eb566f3306b19f1135591ae3e415
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2017-06-30 09:42:26 -0500

    Add validations and associations to Category and Brand models.
    - Add has_many associations to Category and Brand.
    - Add uniqueness validations to Brand model and Brand DB table.
      - Refine uniqueness of name of Brand and Category to be case-sensitive.

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,4 +1,8 @@
 class Brand < ApplicationRecord
+  has_many :products
+
+  # The uniqueness validation will only validate that the object is unique right before being saved, it DOES NOT put a uniqueness constraint on the DB though, you have to do that through a migration.
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
 end
 
 # == Schema Information
@@ -9,4 +13,8 @@ end
 #  name       :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_brands_on_name  (name) UNIQUE
 #
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170627185348) do
+ActiveRecord::Schema.define(version: 20170630143436) do
 
   create_table "brands", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_brands_on_name", unique: true
   end
 
   create_table "categories", force: :cascade do |t|
